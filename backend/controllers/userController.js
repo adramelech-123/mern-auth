aa1a25a59647015d92ffcfdbfbe10544ff9c7915
adramelech-123 <tmamvuto6@gmail.com>
2023-12-17 15:39:14 +0200

    Setup aditional controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,4 +6,26 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
+
+// @desc           Register new user
+// route           POST /api/users
+// @access         Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc           Logout user
+// route           POST /api/users/logout
+// @access         Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc           Get user profile
+// route           GET /api/users/profile
+// @access         Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User profile" });
+});
+
 export { authUser };
