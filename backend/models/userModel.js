0d30bf8fba3a48b0bec551dddaa4f3e4caad8857
adramelech-123 <tmamvuto6@gmail.com>
2023-12-17 20:52:04 +0200

    Register User and userModel password encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
   {
@@ -22,6 +23,26 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// The following middleware intercepts the 'save' operation on a Mongoose schema, checks if the 'password' field has been modified, and if so, hashes the password before allowing the document to be saved in the database. 
+
+// Middleware function triggered before saving a document in the userSchema
+userSchema.pre('save', async function (next) {
+  // Check if the 'password' field has been modified; if not, move to the next middleware or operation
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  // If the 'password' field has been modified:
+  // Generate a salt using bcrypt with a cost factor of 10
+  const salt = await bcrypt.genSalt(10);
+
+  // Hash the password using bcrypt and the generated salt
+  this.password = await bcrypt.hash(this.password, salt);
+
+  // Proceed to the next middleware or save operation
+  next();
+})
 
 const User = mongoose.model('User', userSchema)
 
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,31 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
-import userRoutes from './routes/userRoutes.js'
-
+// Import necessary modules and files
+import express from "express"; // Import the Express framework
+import dotenv from "dotenv"; // Import dotenv for managing environment variables
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"; // Import custom error handling middleware
+import connectDB from "./config/db.js"; // Import the database connection function
+import userRoutes from "./routes/userRoutes.js"; // Import routes for user-related operations
 
 // Configure environment variables
-dotenv.config()
-const port = process.env.PORT || 5001
+dotenv.config(); // Load environment variables from a .env file into process.env
+const port = process.env.PORT || 5001; // Set the server port based on the environment variable or default to 5001
 
-// Create express app
-const app = express()
-connectDB()
+// Create an Express application
+const app = express(); // Initialize the Express app
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+// Connect to the database
+connectDB(); // Call the function to establish a connection to the database
 
-app.use('/api/users', userRoutes)
-app.get('/', (req, res) => res.send('Server is ready!'))
-app.use(notFound)
-app.use(errorHandler)
+// Middleware setup
+app.use(express.json()); // Parse incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies for form submissions
 
-app.listen(port ,() => console.log(`Server started on port ${port}`))
+// Routes setup
+app.use("/api/users", userRoutes); // Mount the user routes at the '/api/users' base path
+app.get("/", (req, res) => res.send("Server is ready!")); // Define a simple route for the root URL that returns 'Server is ready!'
+
+// Error handling middleware setup
+app.use(notFound); // Middleware to handle requests for undefined routes (404 Not Found)
+app.use(errorHandler); // Error handling middleware to handle all types of errors
+
+// Start the server and listen for incoming requests
+app.listen(port, () => console.log(`Server started on port ${port}`)); // Start the server on the specified port and log a message when it starts
